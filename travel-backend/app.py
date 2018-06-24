4b57554aa9aa5411a48e91d9401186516374bff5
Mona Fenzl <oranges_gummibearle@web.de>
2018-06-24 08:35:08 +0200

    changed find-recommended endpoint, model and db stuff

--- travel-backend/app.py
+++ travel-backend/app.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, abort, request, render_template
 from providers.places import get_places
 from database.database import db_session
 from models.restaurant import Restaurant
+from recommender.CB_recommender import recommend_locations_CB, load_matrix
+import json
+import numpy as np
 
 app = Flask(__name__)
 app.config.from_object("config.config.Config")
@@ -9,6 +12,10 @@ app.config.from_object("config.config.Config")
 
 @app.route('/')
 def hello():
+
+    res = Restaurant.query.all()
+    print(res)
+
     return render_template("home.html")
 
 
@@ -21,19 +28,55 @@ def places():
 @app.route('/rest/add-location', methods=['POST'])
 def add_location():
     content = request.get_json()
-    r = Restaurant(content['title'], content['coords']['latitude'], content['coords']['longitude'])
+    r = Restaurant(content['id'], content['title'], content['coords']['latitude'], content['coords']['longitude'], content['google_ratings'], content['types'])
     db_session.add(r)
     db_session.commit()
+    return 'success'
 
 @app.route('/rest/find-recommended', methods=['GET'])
 def find_recommended():
-    location = request.args.get("location")
-    radius = request.args.get("radius")
+    LLM = load_matrix('recommender/data/LLM')
 
-    if location is None or radius is None:
-        abort(400)
-    places = get_places(app.config['API_KEY'], location, radius)
-    return jsonify(places)
+    locations = []
+    with open('recommender/data/test_json.txt', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        for l in data['results']:
+            locations += [l['name']]
+    ULM = np.ones(shape=(15, 8), dtype=np.float32)
+    # ULM = [[1, 1, 1, 1, 1, 1, 0, 0][1, 1, 1, 1, 1, 1, 1, 1][0, 0, 0, 0, 0, 0, 0, 0][1, 0, 0, 0, 1, 0, 0, 0][0, 0, 0, 0, 1, 1, 1, 1][0, 0, 0, 0, 0, 1, 1, 0][1, 0, 0, 0, 0, 0, 0, 0][1, 1, 1, 1, 1, 1, 1, 1]]
+    u = 1
+
+    k = 3
+    r = 2
+    recommendations = recommend_locations_CB(LLM, ULM, u, locations, k, r, OUTPUT=True)
+    names = []
+    names += [locations[r] for r in recommendations]
+    recommendation_record = {}
+    recommendation_records = []
+    latlang = {}
+    with open('recommender/data/test_json.txt', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        for l in data['results']:
+            for j in names:
+                if j == l['name']:
+                    latlang['latitude'] = l['geometry']['location']['lat']
+                    latlang['longitude'] = l['geometry']['location']['lng']
+                    recommendation_record['coords'] = latlang
+                    recommendation_record['title'] = l['name']
+                    recommendation_record['id'] = l['id']
+                    recommendation_record['types'] = l['types']
+                    recommendation_records.append(recommendation_record)
+
+    print(recommendation_records)
+    return 'recommended'
+
+    # location = request.args.get("location")
+    # radius = request.args.get("radius")
+    #
+    # if location is None or radius is None:
+    #     abort(400)
+    # places = get_places(app.config['API_KEY'], location, radius)
+    # return jsonify(places)
 
 
 @app.teardown_appcontext
--- travel-backend/models/restaurant.py
+++ travel-backend/models/restaurant.py
@@ -4,14 +4,19 @@ from database.database import Base
 class Restaurant(Base):
     __tablename__ = 'restaurants'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    name = Column(String(50))
     lat = Column(Float)
-    long = Column(Float)
+    lng = Column(Float)
+    google_ratings = Column(Integer)
+    types =  Column(String(200))
 
-    def __init__(self, name=None, lat=None, long=None):
+    def __init__(self, id=None, name=None, lat=None, lng=None, google_ratings=None, types=None):
+        self.id = id
         self.name = name
         self.lat = lat
-        self.long = long
+        self.lng = lng
+        self.google_ratings
+        self.types
 
     def __repr__(self):
         return '<Restaurant %r>' % (self.name)
