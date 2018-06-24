57670bac7f2cc91ccb277b95ac9d90a8f82ec05f
TomasDavidYe <tommygamba17@gmail.com>
2018-06-24 05:20:24 +0200

    IT IS IN AGAIN...

--- travel-backend/providers/database_generator.py
+++ travel-backend/providers/database_generator.py
@@ -4,7 +4,8 @@ import recommender.model_classes.Restaurant as Rest
 
 conn = sqlite3.connect('restaurant.db')
 c = conn.cursor()
-c.execute("SELECT * FROM users")
+
+
 print(c.fetchall())
 conn.commit()
 conn.close()
--- travel-backend/providers/places.py
+++ travel-backend/providers/places.py
@@ -43,3 +43,45 @@ class GooglePlacesDataProvider(PlacesDataProvider):
 
 
         return places
+
+
+
+#
+# restaurant_list = []
+# caller = GooglePlacesDataProvider()
+# rating = ''
+# for i in range(-2, 2):
+#     for j in range(-2, 2):
+#         places = caller.get_places("AIzaSyDS40mCvePWcb9_eI_SErFQpt98UoTI3UI",
+#                                    str(50.084062 + i / 100) + ', ' + str(14.421809 + j / 100),
+#                                    "500")
+#         for k in range(0, 59):
+#             ident = places[k]['id']
+#             x = places[k]['geometry']['location']['lat']
+#             y = places[k]['geometry']['location']['lat']
+#             name = places[k]['name']
+#             if places[k].__contains__('rating'):
+#                 rating = places[k]['rating']
+#             types = ','.join(places[k]['types'])
+#             restaurant_list.append(Rest.Restaurant(ident, name, x, y, rating, types))
+#
+# restaurant_dictionary = dict()
+# for restaurant in restaurant_list:
+#     if not restaurant_dictionary.__contains__(restaurant.id):
+#         restaurant_dictionary[restaurant.id] = restaurant
+#
+# for key in restaurant_dictionary.keys():
+#     print(restaurant_dictionary[key].name)
+#
+# print(len(restaurant_dictionary))
+#
+# for key in restaurant_dictionary.keys():
+#     A = restaurant_dictionary[key]
+#     c.execute("""
+#         INSERT INTO restaurants VALUES (:id, :lat, :lng, :name, :google_ratings, :types)
+#             """, {'id': A.id, 'lat': A.lat, 'lng': A.lng, 'name': A.name, 'google_ratings': A.google_rating, 'types': A.types})
+conn = sqlite3.connect('restaurant.db')
+c = conn.cursor()
+
+conn.commit()
+conn.close()
